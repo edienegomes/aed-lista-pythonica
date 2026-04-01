8bbeec0aa4ccc4881d201ea41428b14b2574c17a
Ediene <edienegomes44@gmail.com>
2026-04-01 16:10:00 -0300

    exercicio 4 concluido

--- src/exercicio_04.py
+++ src/exercicio_04.py
@@ -1,11 +1,6 @@
+#Retorna o nome em diferentes formatos: (lowercase, uppercase, titlecase)
+# minúsculo, maiúsculo, capitalizado 
 def format_name(name: str) -> tuple[str, str, str]:
-    """
-    Retorna o nome em diferentes formatos.
-
-    Args:
-        name (str): nome de entrada
-
-    Returns:
-        tuple[str, str, str]: (lowercase, uppercase, titlecase)
-    """
-    pass
+    nome = name
+    
+    return (nome.lower(), nome.upper(), nome.capitalize())
