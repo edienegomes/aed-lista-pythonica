cf705b27189f286aa3ce78cd267ca294ed217b02
Ediene <edienegomes44@gmail.com>
2026-04-01 19:47:32 -0300

    exercicio 14 concluido

--- src/exercicio_14.py
+++ src/exercicio_14.py
@@ -1,11 +1,8 @@
+#Gera uma lista de números de 1 até n.
 def generate_numbers(n: int) -> list[int]:
-    """
-    Gera uma lista de números de 1 até n.
+    lista = []
 
-    Args:
-        n (int): limite superior
+    for i in range (1, n + 1):
+        lista.append(i)
 
-    Returns:
-        list[int]: lista [1, 2, ..., n]
-    """
-    pass
+    return lista
